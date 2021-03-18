55a0144f52b0f5dda4442d04abdfdd9838ea8ee1
devbtech <youthpassion.com@gmail.com>
2021-03-18 21:05:51 +0500

    Added functionality to display language dropdown even if user is logged out. Breakdown components in Navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,69 +10,10 @@ import { Languages } from '../../constants/index'
 /**
  * Component for Rendering the NavBar
  * @param user
- * @constructor
  *
  */
 export default function NavBar ({ user }) {
-  const [language, setLanguage] = useState(Languages.ENGLISH)
-  const { t, i18n } = useTranslation()
-  const dispatch = useDispatch()
-  const logOut = () => {
-    dispatch(doLogout())
-  }
-
-  const changeLanguage = (lang) => {
-    setLanguage(lang)
-    i18n.changeLanguage(lang)
-  }
-
-  const userDropDown = (user) => {
-    if (user) {
-      return (
-        <>
-          <div className="App container">
-            <DropdownButton
-              alignRight
-              title={
-                language === Languages.ENGLISH
-                  ? t('Navbar.english')
-                  : t('Navbar.french')
-              }
-              id="dropdown-menu-align-right"
-              onSelect={changeLanguage}
-            >
-              <Dropdown.Item eventKey={Languages.ENGLISH}>
-                {t('Navbar.english')}
-              </Dropdown.Item>
-              <Dropdown.Item eventKey={Languages.FRENCH}>
-                {t('Navbar.french')}
-              </Dropdown.Item>
-            </DropdownButton>
-            {/* <h4>You selected {value}</h4> */}
-          </div>
-
-          <NavDropdown
-            title={user.firstName}
-            id="basic-nav-dropdown"
-            className="ml-auto"
-          >
-            <NavDropdown.Item href="/changepassword">
-              {t('Navbar.settings')}
-            </NavDropdown.Item>
-            <NavDropdown.Item href="/profile">
-              {t('Navbar.profile')}
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#" onClick={() => logOut()}>
-              {t('Navbar.logOut')}
-            </NavDropdown.Item>
-          </NavDropdown>
-        </>
-      )
-    } else {
-      return <Nav.Link href="/login">{t('Navbar.logIn')}</Nav.Link>
-    }
-  }
+  const { t } = useTranslation()
 
   return (
     <Navbar expand="lg" variant="dark" className="site-header">
@@ -82,8 +23,83 @@ export default function NavBar ({ user }) {
         <Nav className="mr-auto">
           <Nav.Link href="#home">{t('Navbar.home')}</Nav.Link>
         </Nav>
-        <Nav className="ml-auto">{userDropDown(user)}</Nav>
+        <Nav className="ml-auto">
+          <UserInfoDropdown user={user} />
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
+  )
+}
+
+const UserInfoDropdown = ({ user }) => {
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+
+  const logOut = () => {
+    dispatch(doLogout())
+  }
+
+  if (user) {
+    return (
+      <React.Fragment>
+        <div className="App container">
+          <LanguageDropdown />
+        </div>
+
+        <NavDropdown
+          title={user.firstName}
+          id="basic-nav-dropdown"
+          className="ml-auto"
+        >
+          <NavDropdown.Item href="/changepassword">
+            {t('Navbar.settings')}
+          </NavDropdown.Item>
+          <NavDropdown.Item href="/profile">
+            {t('Navbar.profile')}
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#" onClick={logOut}>
+            {t('Navbar.logOut')}
+          </NavDropdown.Item>
+        </NavDropdown>
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <LanguageDropdown />
+        <Nav.Link href="/login">{t('Navbar.logIn')}</Nav.Link>
+      </React.Fragment>
+    )
+  }
+}
+
+const LanguageDropdown = () => {
+  const { t, i18n } = useTranslation()
+  const [language, setLanguage] = useState(Languages.ENGLISH)
+
+  const changeLanguage = (lang) => {
+    setLanguage(lang)
+    i18n.changeLanguage(lang)
+  }
+
+  return (
+    <DropdownButton
+      alignRight
+      title={
+        language === Languages.ENGLISH
+          ? t('Navbar.english')
+          : t('Navbar.french')
+      }
+      id="dropdown-menu-align-right"
+      onSelect={changeLanguage}
+    >
+      <Dropdown.Item eventKey={Languages.ENGLISH}>
+        {t('Navbar.english')}
+      </Dropdown.Item>
+      <Dropdown.Item eventKey={Languages.FRENCH}>
+        {t('Navbar.french')}
+      </Dropdown.Item>
+    </DropdownButton>
   )
 }

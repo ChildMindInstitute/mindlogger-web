444ace7a6be452d398ef888df297b51383cc7f50
devbtech <youthpassion.com@gmail.com>
2021-03-19 18:50:56 +0500

    Fixed footer design; Added prettier configuration; Fixed dropdown design bug

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer>
-      <p>
-        <img src="https://27c2s3mdcxk2qzutg1z8oa91-wpengine.netdna-ssl.com/wp-content/themes/childmind/assets/img/cmi-logo-vert-ko.svg" />
-        © <a href="https://childmind.org">Child Mind Institute </a> MATTER Lab
+    <footer className="d-flex justify-content-center align-items-center mt-auto bg-light pt-3 pb-3 mt-3 footer">
+      <p className="mt-3 d-flex align-items-center">
+        <img
+          className="logo mr-1"
+          src="https://27c2s3mdcxk2qzutg1z8oa91-wpengine.netdna-ssl.com/wp-content/themes/childmind/assets/img/cmi-logo-vert-ko.svg"
+        />
+        © <a href="https://childmind.org" className="mx-1"> Child Mind Institute </a> MATTER Lab
         2019
       </p>
     </footer>
--- src/index.js
+++ src/index.js
@@ -1,49 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import moment from 'moment';
-import "./i18Next";
-import  { Suspense } from "react";
-import { Provider } from 'react-redux';
-import configureStore from './store';
-import { clearUser } from './state/user/user.actions';
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import moment from 'moment'
+import './i18Next'
+import { Provider } from 'react-redux'
+import configureStore from './store'
+import { clearUser } from './state/user/user.actions'
 
-import './custom.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './index.css';
+import './custom.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './index.css'
 
-import App from './App';
+import App from './App'
 
-import * as serviceWorker from './serviceWorker';
-
+import * as serviceWorker from './serviceWorker'
 
 const checkAuthToken = (store) => {
-  const state = store.getState();
-  const auth = state.user.auth;
-  const expirationDate = moment(auth.expires);
+  const state = store.getState()
+  const auth = state.user.auth
+  const expirationDate = moment(auth.expires)
 
   if (auth === null || moment().isAfter(expirationDate)) {
-    store.dispatch(clearUser()); // Just in case
-    return false;
+    store.dispatch(clearUser()) // Just in case
+    return false
   }
 
-  return true;
-};
+  return true
+}
 
-
-const store = configureStore();
+const store = configureStore()
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Suspense fallback={null}>
-      <App />
+      <Suspense fallback={null}>
+        <App />
       </Suspense>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()

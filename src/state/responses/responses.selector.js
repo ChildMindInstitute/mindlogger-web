49884426dd0744e752e04b368243812fcc467619
mtg137 <jony374@outlook.com>
2021-05-24 10:12:53 -0400

    update to responses

--- src/state/responses/responses.selector.js
+++ src/state/responses/responses.selector.js
@@ -1,27 +1,27 @@
 import { createSelector } from "reselect";
 import * as R from "ramda";
 
-export const responsesSelector = R.path(["response", "responseHistory"]);
+export const responsesSelector = R.path(["responses", "responseHistory"]);
 
-export const uploadQueueSelector = R.path(["response", "uploadQueue"]);
+export const uploadQueueSelector = R.path(["responses", "uploadQueue"]);
 
 export const isDownloadingResponsesSelector = R.path([
-  "response",
+  "responses",
   "isDownloadingResponses",
 ]);
 
-export const isSummaryScreenSelector = R.path(["response", "isSummaryScreen"]);
+export const isSummaryScreenSelector = R.path(["responses", "isSummaryScreen"]);
 
 export const downloadProgressSelector = R.path([
-  "response",
+  "responses",
   "downloadProgress",
 ]);
 
-export const inProgressSelector = R.path(["response", "inProgress"]);
+export const inProgressSelector = R.path(["responses", "inProgress"]);
 
-export const activityOpenedSelector = R.path(["response", "activityOpened"]);
+export const activityOpenedSelector = R.path(["responses", "activityOpened"]);
 
-export const responseScheduleSelector = R.path(["response", "schedule"]);
+export const responseScheduleSelector = R.path(["responses", "schedule"]);
 
 export const currentAppletResponsesSelector = createSelector(
   responsesSelector,

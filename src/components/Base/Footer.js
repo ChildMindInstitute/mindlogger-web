8c9d5a047159eff8373028a39316a77cd5985159
Matchdev120 <bestmatch19940123@gmail.com>
2021-05-28 08:41:13 -0400

    Fix footer styles

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="d-flex justify-content-center align-items-center mt-auto bg-light pt-3 pb-3 mt-3 footer">
-      <p className="mt-3 d-flex align-items-center">
+      <p className="mt-3 text-center">
         <img
           className="logo mr-1"
           src="https://27c2s3mdcxk2qzutg1z8oa91-wpengine.netdna-ssl.com/wp-content/themes/childmind/assets/img/cmi-logo-vert-ko.svg"

359124cce9165fe411b4ef69deb5af4404ca9f29
Matchdev120 <bestmatch19940123@gmail.com>
2021-06-02 16:42:15 -0400

    Modify eslint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,8 @@
 module.exports = {
-  printWidth: 100,
   tabWidth: 2,
-  semi: true,
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: "always",
-  proseWrap: "preserve",
-  overrides: [
-    {
-      files: '*.{js,jsx,tsx,ts,scss,json,html}',
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
+  printWidth: 120,
 };

2e35119849d1e4df662da4ec2dac1802713ed649
Matchdev120 <bestmatch19940123@gmail.com>
2021-06-02 08:16:51 -0400

    Modify prettier settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,19 @@
 module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: "always",
-  proseWrap: "preserve"
+  proseWrap: "preserve",
+  overrides: [
+    {
+      files: '*.{js,jsx,tsx,ts,scss,json,html}',
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };

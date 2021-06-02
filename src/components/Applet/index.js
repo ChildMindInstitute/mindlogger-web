20100704ff92e5afdc61546bcaf9282023e5d0d7
Matchdev120 <bestmatch19940123@gmail.com>
2021-06-02 08:09:04 -0400

    Add some blank space

--- src/components/Applet/index.js
+++ src/components/Applet/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Applet = () => (
+
+
+
   <Link to="/applet/43yy4">
     <div className="card hover" style={{ width: "15rem" }}>
       <div className="pr-4 pl-4 pt-4">
         <img src="logo192.png" className="card-img-top rounded border" alt="applet-image" />
       </div>
       <div className="card-body">
-        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p className="card-text">        Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
     </div>
   </Link>

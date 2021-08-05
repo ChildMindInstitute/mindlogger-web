0eed4eacb568505f31bace1422a6585266cb57dd
mtg137 <jony374@outlook.com>
2021-08-05 12:43:59 -0400

    use new markdown in items

--- src/components/Screens/Markdown.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import gfm from "remark-gfm";
-import ReactMarkdown from "react-markdown";
-
-export default ({ children }) => (
-  <ReactMarkdown remarkPlugins={[gfm]} children={children.replace(/(!\[.*\]\s*\(.*?) =\d*x\d*(\))/g, '$1$2')} />
-)

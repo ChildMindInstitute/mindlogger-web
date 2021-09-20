756cee3781e29e8750f747367919dbbb3b14a462
devbtech <devtech728@gmail.com>
2021-09-21 01:03:45 +0500

    CMI logo is changed

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
       <p className="mt-3 text-center">
         <img
           className="logo mr-1"
-          src="https://27c2s3mdcxk2qzutg1z8oa91-wpengine.netdna-ssl.com/wp-content/themes/childmind/assets/img/cmi-logo-vert-ko.svg"
+          src="https://lh3.googleusercontent.com/a7voPrdWNxlivOBo3sXrLDFU-Xk17xiMyZCXW7wNJ6wtrEi12YGAMkAMQivk6BpkCLbcmBUEJYRdFQW4mZ6Sx6loqc5PsdCBdTu-4p7gKZD8FyA2SLQHkUuGiwYJc4BcG4dY1hVLKjxwo7GPDYxnotJ9dNib3KJT5SXWZUJ3MEk3petkQboCVCS5yO480EB9T0Er93lrSu7u9eC4ajDAkqysnXWXU5zTG9ifg6KCct29cY7OqlfIl9p78UA9-S5raiL7MX9oMntNOUYvf4uSWQXVm_xXnKRlAG_YaibFHXt-RQTQiJhsR8iVjbdK9McHqHoMEI5GtBRONscOARd-cUDZ9qA_zTs1PNnDqb7JiUQYzeSWz4gdssu8Rfa7oAtB8CehkNd0TfiIjdcM_9LCeo91WqKDmQAv5qPUsi2HruDu4bL-1MxUr6ay1Tx35CmMAI30C6FGENA0xbMSsk5bxHsvG8XyrQoX_M461VYzyGDp9JFtpXc-EtfVrj8_l_WfetMpvGPTtxtumWrtkfu1Wcgnvk3T8FtSEVaEJOYwhS6n7juR9Vs8pWWMIOL7mit7TJ4nFh_Sl5s7n5YtmcPpAvQuizRTSSTKTQBzm_atx5-mQJ6V1Lk26EPdPZfjDnXCFJho9IMR0xECHcq0s0Q-EW-E61h2t-igKxpWk3qgBAHN8rGqyhvhR9fbJW69W2um9VgIUiCvgIDo8JwipyigkwQ=w103-h109-no?authuser=2"
         />
         ©{' '}
         <a

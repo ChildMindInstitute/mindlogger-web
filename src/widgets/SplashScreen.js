4ed79b1b3e7b0f725eab1b53e0676c22ae52e25f
mtg137 <jony374@outlook.com>
2021-11-18 11:36:18 -0500

    show gif files as static image if not active

--- src/widgets/SplashScreen.js
+++ src/widgets/SplashScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import _ from "lodash";
 import {
   Row,
@@ -21,6 +21,8 @@ const SplashScreen = (props) => {
     splashScreen
   } = props;
 
+  const splashImage = useRef();
+
   let isImageSplash = false;
 
   if (splashScreen.includes(".png")
@@ -30,6 +32,31 @@ const SplashScreen = (props) => {
     || splashScreen.includes(".jpeg")) {
     isImageSplash = true;
   }
+
+  function freeze_gif(i) {
+    var c = document.createElement('canvas');
+    var w = c.width = i.offsetWidth;
+    var h = c.height = i.offsetHeight;
+    c.getContext('2d').drawImage(i, 0, 0, w, h);
+
+    for (var j = 0, a; a = i.attributes[j]; j++) {
+      c.setAttribute(a.name, a.value);
+    }
+    i.parentNode.replaceChild(c, i);
+  }
+
+  useEffect(() => {
+    if (splashScreen.includes('.gif') && !isNextShown) {
+      const gif = splashImage.current.querySelector('img');
+      if (gif) {
+        if (gif.complete) {
+          freeze_gif(gif);
+        } else {
+          gif.addEventListener('load', () => freeze_gif(gif))
+        }
+      }
+    }
+  }, [isNextShown, splashScreen])
 
   return (
     <Card className="mb-3" style={{ maxWidth: "auto" }}>
@@ -44,8 +71,11 @@ const SplashScreen = (props) => {
             </Card.Title>
             <div className="no-gutters">
               {isImageSplash ? (
-                <div className="splash-container">
-                  <img src={splashScreen} className="image-splash" />
+                <div ref={splashImage} className="splash-container">
+                  <img
+                    src={splashScreen}
+                    className="image-splash"
+                  />
                 </div>
               ): (
                 <div className="splash-container">

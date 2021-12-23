848e1a5280ca5e64f2a9e6154a8d598fe485ad82
binaryExDev <binarydev51@gmail.com>
2021-12-23 06:58:05 -0500

    Center footer text

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="d-flex justify-content-center align-items-center bg-light footer">
-      <p className="mt-3 text-center">
+      <p className="mt-3 text-center d-flex align-items-center">
         <img
           className="logo mr-1"
           src="https://cmi-logos.s3.amazonaws.com/ChildMindInstitute_Logo_Horizontal_RGB.png"
         />
-        ©{' '}
+        {'© '}
         <a
           href="https://childmind.org"
           className="mx-1"

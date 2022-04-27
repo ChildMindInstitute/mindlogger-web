27907e1980e398cf74336f29787186ed839183f2
devbtech <devtech728@gmail.com>
2022-04-27 17:33:48 +0500

    [FIX][UI] 'Child mind Institute' and 'Terms of Service' links are missing in mobile view

--- src/components/Base/Footer.js
+++ src/components/Base/Footer.js
@@ -5,22 +5,26 @@ export const Footer = () => {
     <footer className="d-flex justify-content-center align-items-center bg-light footer">
       <p className="mt-3 text-center d-flex align-items-center">
         <img
-          className="logo mr-1"
+          className="logo mr-1 xs-display-none"
           src="https://cmi-logos.s3.amazonaws.com/ChildMindInstitute_Logo_Horizontal_RGB.png"
         />
+        <span className="xs-display-none">
         {'© '}
+        </span>
         <a
           href="https://childmind.org"
-          className="mx-1 xs-display-none"
+          className="mx-1"
           target={'_blank'}
           rel="noreferrer"
         >
           {' '}
           Child Mind Institute{' '}
         </a>{' '}
+        <span className="xs-display-none">
         MATTER Lab 2022
+        </span>
 
-        <a className="mx-4 xs-display-none" href="https://mindlogger.org/terms" target='_blank'>Terms of Service</a>
+        <a className="mx-4" href="https://mindlogger.org/terms" target='_blank'>Terms of Service</a>
       </p>
     </footer>
   )

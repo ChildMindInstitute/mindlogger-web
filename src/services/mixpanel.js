08e4801c70f595f3382c20f3c5b774ce855a963f
Mironov, Artem <amironov@scnsoft.com>
2023-08-29 12:47:40 +0300

    feat(mixpanel): Added events for login and assessment started

--- src/services/mixpanel.js
+++ src/services/mixpanel.js
@@ -1,23 +1,20 @@
 import mixpanel from 'mixpanel-browser';
 import UserAgentParser from 'ua-parser-js';
 
-mixpanel.init('YOUR_MIXPANEL_TOKEN');
-
 let isProduction = process.env.NODE_ENV === 'production';
 
 export let Mixpanel = {
   init() {
-    this.track('INFO', {
-      os: new UserAgentParser().getOS().name,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
+    if (isProduction) mixpanel.init('075d1512e69a60bfcd9f7352b21cc4a2');
   },
   trackPageView(pageName) {
-    if (isProduction) mixpanel.track_pageview('[Legacy][Web] ' + pageName);
+    if (isProduction) mixpanel.track_pageview({ page: '[Legacy][Web] ' + pageName });
   },
   track(action, payload) {
     if (isProduction) mixpanel.track('[Legacy][Web] ' + action, payload);
+  },
+  login(userId) {
+    if (isProduction) mixpanel.identify(userId);
   },
   logout() {
     mixpanel.reset();
